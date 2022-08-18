b6363d499a50a33dc4482ad62dd7b04a9e59f9b0
Siravit Suwimol <siravit4work@gmail.com>
2022-08-18 11:23:56 +0700

    frist commit
    frist commit

---
+++ backend/interceptor.js
@@ -0,0 +1,16 @@
+module.exports = {
+  verify1: (req, res, next) => {
+    if (req.query.token === "1234") {
+      next();
+    } else {
+      res.end("No autorized1");
+    }
+  },
+  verify2: (req, res, next) => {
+    if (req.query.level === "admin") {
+      next();
+    } else {
+      res.end("Level is not enough");
+    }
+  },
+};
